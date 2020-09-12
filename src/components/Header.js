13449ddce466b2d2de5f8b7066b95c12613890f5
Kirill Mecker <ghost3299@yandex.ru>
2020-09-12 18:59:40 +0400

    feat: add header and footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,9 @@
 import React from "react";
+import headerLogo from "../images/header/logo.svg";
 function Header() {
   return (
     <header className="header">
-      <img
-        className="header__logo"
-        src="./images/header/logo.svg"
-        alt="Место Россия"
-      />
+      <img className="header__logo" src={headerLogo} alt="Место Россия" />
     </header>
   );
 }
