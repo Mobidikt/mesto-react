48d88013287c2462b29b02f2458eb81620617eb9
Kirill Mecker <ghost3299@yandex.ru>
2020-09-28 11:26:22 +0400

    Updates

---
+++ precache-manifest.8621802749416ab8f3393acd5361f66d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37af24806ba8ab7e445500f42f461c00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6d148173ef6176cb711",
+    "url": "/static/css/main.7bc45602.chunk.css"
+  },
+  {
+    "revision": "352b0a0cbbc0c78ebb05",
+    "url": "/static/js/2.602e3a58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.602e3a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d148173ef6176cb711",
+    "url": "/static/js/main.7b991754.chunk.js"
+  },
+  {
+    "revision": "9faa06e49578191d5a13",
+    "url": "/static/js/runtime-main.66dd7950.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/static/media/add.bdfb8ebb.svg"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/static/media/close.c2d0901f.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/static/media/eddit.58afbe73.svg"
+  },
+  {
+    "revision": "34c4685038e8cf551566cb4ee96e20e5",
+    "url": "/static/media/edit_profile.34c46850.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/static/media/group.6c74d9b5.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/static/media/like_activ.9fdebb6b.svg"
+  },
+  {
+    "revision": "0fce8a33439af0f3b7fa40bc3c26006e",
+    "url": "/static/media/logo.0fce8a33.svg"
+  }
+]);
